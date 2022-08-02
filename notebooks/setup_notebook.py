f8a2989d017c702817e126db51413bc205775192
joshdidcote <joshua.didcote@outlook.com>
2022-08-02 10:44:18 +0100

    Update setup_notebook.py to use sys path instead of os.chdir

--- notebooks/setup_notebook.py
+++ notebooks/setup_notebook.py
@@ -1,10 +1,2 @@
-import os
-from pathlib import Path
-
-
-def set_path_to_root():
-    root_dir = Path(os.path.abspath('')).parent
-    os.chdir(root_dir)
-
-
-set_path_to_root()
+import sys
+sys.path.append("../")
